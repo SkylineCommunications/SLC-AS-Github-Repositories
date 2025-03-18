e97186bf2c23ad4983ea94afc96a3a7abbb1a7e5
BlakeSM <blake.smethers@skyline.be>
2025-03-18 07:33:30 -0500

    execute workflow rough draft

---
+++ Github Repositories Execute Workflow_1/Extensions.cs
@@ -0,0 +1,25 @@
+﻿// Ignore Spelling: Github
+
+namespace Github_Repositories_Extensions
+{
+	using System;
+
+	using Skyline.DataMiner.Core.DataMinerSystem.Common;
+
+	public static class Extensions
+	{
+		public static bool TryGetRow(this IDmsTable table, string rowKey, out object[] row)
+		{
+			try
+			{
+				row = table.GetRow(rowKey);
+				return true;
+			}
+			catch (Exception)
+			{
+				row = null;
+				return false;
+			}
+		}
+	}
+}
